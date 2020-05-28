3b62fe00859302cb22c02ffed5c5b7378e3ad2c9
smadinen <smadine@conviva.com>
2020-05-28 14:08:30 +0530

    Release 4.0.0

--- DemoApp/DemoApp/ViewController.m
+++ DemoApp/DemoApp/ViewController.m
@@ -1,13 +1,15 @@
 #import "ViewController.h"
-@import ConvivaCore;
 @import ConvivaAVFoundation;
+@import ConvivaSDK;
 
 
 @interface ViewController ()
 @property(nonatomic,strong) AVPlayerViewController *avPlayerViewController;
 @property(nonatomic,strong) AVPlayer *avPlayer;
 @property(nonatomic,strong) UIView *playerView;
-@property(nonatomic,strong) ConvivaLightSession *session;
+@property(nonatomic,strong) CISAnalytics analytics;
+@property(nonatomic,strong) CISVideoAnalytics videoAnalytics;
+
 @end
 
 @implementation ViewController
@@ -25,8 +27,10 @@
 
 - (IBAction)stopVideo:(id)sender{
     self.avPlayer.rate = 0.0;
-    [self.session cleanup];
-    self.session = nil;
+    [self.videoAnalytics cleanup];
+    [self.analytics cleanup];
+    self.videoAnalytics = nil;
+    self.analytics = nil;
     [self.avPlayerViewController.view removeFromSuperview];
     self.avPlayer = nil;
 }
@@ -43,25 +47,25 @@
 }
 
 -(void)startPlayback{
-    
-    ConvivaContentInfo *contentInfo = [ConvivaContentInfo createInfoForLightSessionWithAssetName:@"Aplos-combined"];
-    contentInfo.isLive = NO;
-    //contentInfo.isOfflinePlayback = YES;
+    NSMutableDictionary *contentInfo = [[NSMutableDictionary alloc] init];
+    [contentInfo setValue:@"Simplified-SDK" forKey:CIS_SSDK_METADATA_ASSET_NAME];
+    [contentInfo setValue:@NO forKey:CIS_SSDK_METADATA_IS_LIVE];
     AVPlayerItem *item =[[AVPlayerItem alloc] initWithURL:[[NSURL alloc] initWithString:@"http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8"]];
     self.avPlayer = [[AVPlayer alloc] initWithPlayerItem:item];
     self.avPlayerViewController = [[AVPlayerViewController alloc] init];
     self.avPlayerViewController.delegate = self;
     self.avPlayerViewController.player = self.avPlayer;
-    self.session = [LivePass createSessionWithStreamer:self.avPlayer andConvivaContentInfo:contentInfo];
-
+    [self.videoAnalytics setPlayer:self.avPlayer];
     [self addChildViewController:self.avPlayerViewController];
     [self.view addSubview:self.avPlayerViewController.view];
     self.avPlayerViewController.view.frame = self.playerView.frame;
     [self.avPlayer play];
+    [self.videoAnalytics reportPlaybackRequested:contentInfo];
 }
 
 -(void)initConvivaLib{
-    [LivePass initWithCustomerKey: @"CUSTOMER_ID"
-                      andSettings: @{ @"gatewayUrl": @"TOUCHSTONE_URL" }];
+    self.analytics = [CISAnalyticsCreator createWithCustomerKey:@"PRODUCTION_CUSTOMER_KEY"];
+    self.videoAnalytics = [self.analytics createVideoAnalytics];
 }
+
 @end
