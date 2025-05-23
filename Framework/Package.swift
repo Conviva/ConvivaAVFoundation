54d927dd9d7f544be2190825e798721d5724f012
Butchi Peddi <bpeddi@conviva.com>
2025-05-23 20:25:40 +0530

    Release 4.2.3

--- Framework/Package.swift
+++ Framework/Package.swift
@@ -1,37 +1,24 @@
-// swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-import PackageDescription
-
 let package = Package(
-    name: "ConvivaAVFoundation",
-    platforms: [
-        .iOS(.v12),
-        .tvOS(.v12)
-    ],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "ConvivaAVFoundation",
-            targets: ["ConvivaAVFoundationTarget"])
-    ],
-    targets: [
-        .binaryTarget(
-            name: "ConvivaAVFoundation",
-            url: "https://github.com/Conviva/ConvivaAVFoundation/raw/4.2.2/Framework/ConvivaAVFoundation.xcframework.zip",
-            checksum: "d6e26b290277f4ed6e8077104664cef40b79d3e67018d28751eb0e24432f8968"),
-        
-        .binaryTarget(
-            name: "ConvivaSDK",
-            url: "https://github.com/Conviva/ConvivaSDK/raw/4.2.2/Framework/ConvivaSDK.xcframework.zip",
-            checksum: "49e3aea9df405132cecd4d73c2648399340553a6a8ba3192699549a5947e2159"),
+  name: "ConvivaAVFoundation",
+  platforms: [
+    .iOS(.v12),
+    .tvOS(.v12)
+  ],
+  products: [
+    .library(
+      name: "ConvivaAVFoundation",
+      targets: ["ConvivaAVFoundation", "ConvivaSDK"]
+    )
+  ],
+  targets: [
+    .binaryTarget(
+      name: "ConvivaAVFoundation",
+      url: "https://github.com/Conviva/ConvivaAVFoundation/raw/4.2.3/Framework/ConvivaAVFoundation.xcframework.zip",
+      checksum: "2f6a2030ae3f2c62a1b9676ee157b5587a1fad7b24e708310358283b4f3f394e"),
 
-        .target(
-              name: "ConvivaAVFoundationTarget",
-              dependencies: [
-                .target(name: "ConvivaSDK"),
-                .target(name: "ConvivaAVFoundation")
-              ],
-              path: "PlatformExcludes"
-            )
-     ]
+      .binaryTarget(
+        name: "ConvivaSDK",
+        url: "https://github.com/Conviva/ConvivaSDK/raw/4.2.3/Framework/ConvivaSDK.xcframework.zip",
+        checksum: "f544a223a8178e92c8517fd51805139e70725929a91785465da0f98e9201e384")
+  ]
 )
